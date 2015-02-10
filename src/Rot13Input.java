491364a8cc558fc626008f357597c054f51271d6
Eric Wu <eric.ics211@gmail.com>
2015-02-09 14:38:56 -1000

    Answer to rot13input

--- src/Rot13Input.java
+++ src/Rot13Input.java
@@ -1,28 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 // This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
 // Copyright (c) 1997 by David Flanagan
 // This example is provided WITHOUT ANY WARRANTY either expressed or implied.
 // You may study, use, modify, and distribute it for non-commercial purposes.
 // For any commercial use, see http://www.davidflanagan.com/javaexamples
 
-import java.io.*;  // We're doing input, so import I/O classes
+
 
 /**
- * This program reads lines of text from the user, encodes them using the 
- * trivial "Rot13" substitution cipher, and then prints out the encoded lines.
- **/
+ * Reads lines of text from the user, then prints out the encoded lines.
+ */
 public class Rot13Input {
+  /**
+   * A command line executable that accepts input to print out lines using the Rot13 cipher
+   *
+   * @param args contains the user input from command line
+   * @throws IOException
+   */
   public static void main(String[] args) throws IOException {
     // Get set up to read lines of text from the user
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    for(;;) {                                    // Loop forever
-      System.out.print("> ");                    // Print a prompt
-      String line = in.readLine();               // Read a line
-      if ((line == null) || line.equals("quit")) // If EOF or "quit" then...
-        break;                                   // ... break out of the loop
-      StringBuffer buf = new StringBuffer(line); // Convert to a StringBuffer
-      for(int i = 0; i < buf.length(); i++)      // For each character...
-        buf.setCharAt(i, rot13(buf.charAt(i)));  //   read, encode, put it back
-      System.out.println(buf);                   // Print encoded line
+    for (;;) {                                      // Loop forever
+      System.out.print("> ");                       // Print a prompt
+      String line = in.readLine();                  // Read a line
+      if ((line == null) || line.equals("quit")) {  // If EOF or "quit" then...
+        break;                                      // ... break out of the loop
+      }
+      StringBuffer buf = new StringBuffer(line);    // Convert to a StringBuffer
+      for (int i = 0; i < buf.length(); i++) {      // For each character...
+        buf.setCharAt(i, rot13(buf.charAt(i)));     //   read, encode, put it back
+      }
+      System.out.println(buf);                      // Print encoded line
     }
   }
 
@@ -30,16 +41,21 @@ public class Rot13Input {
    * This method performs the Rot13 substitution cipher.  It "rotates"
    * each letter 13 places through the alphabet.  Since the Latin alphabet
    * has 26 letters, this method both encodes and decodes.
+   *
+   * @param c the plain text character to encode
+   * @return the encoded character
    **/
   public static char rot13(char c) {
     if ((c >= 'A') && (c <= 'Z')) {  // For uppercase letters
       c += 13;                       // Rotate forward 13
-      if (c > 'Z') c -= 26;         // And subtract 26 if necessary
+      if (c > 'Z') c -= 26;          // And subtract 26 if necessary
     }
+
     if ((c >= 'a') && (c <= 'z')) {  // Do the same for lowercase letters
       c += 13;
       if (c > 'z') c -= 26;
     }
+
     return c;                        // Return the modified letter
   }
 }
---
+++ src/package-info.java
@@ -0,0 +1,3 @@
+/**
+ * Created by Eric on 2/9/2015.
+ */
